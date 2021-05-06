50215804deef3d2e99fd0b7496b36d57efd871f4
lpattis1 <lpattis1@asu.edu>
2021-05-06 10:15:23 -0500

    Added an animation to the movies when clicked, cleaned up the style, and changed text color when each individual genre is selected.

--- app.js
+++ app.js
@@ -1,22 +1,88 @@
 "use strict";
+
+// Global variables:
 const genres = document.querySelectorAll(".genre");
 const movies = document.querySelectorAll(".movie");
-let moviesSelected = [];
 
-genres.forEach(function (genre) {
-  genre.addEventListener("click", function (e) {
-    e.preventDefault();
+// Filtering through movies function:
+function filterMovies() {
+  genres.forEach(function (genre) {
+    genre.addEventListener("click", function (e) {
+      // Preventing default to allow the classes to change and be removed:
+      e.preventDefault();
+      let genreType = genre.textContent.toLowerCase();
 
-    let genreType = genre.textContent.toLowerCase();
-    movies.forEach((movie) => {
-      if (movie.classList.contains(genreType)) {
-        movie.style.display = "block";
-        console.log(movie);
-      } else if (genreType === "all") {
-        movie.style.display = "block";
-      } else {
-        movie.style.display = "none";
+      //   Changing the font color of each selected genre:
+      if (genre === genres[0]) {
+        genre.classList.add("active-btn");
+        genres[1].classList.remove("active-btn");
+        genres[2].classList.remove("active-btn");
+        genres[3].classList.remove("active-btn");
+        genres[4].classList.remove("active-btn");
+        genres[5].classList.remove("active-btn");
+      } else if (genre === genres[1]) {
+        genre.classList.add("active-btn");
+        genres[0].classList.remove("active-btn");
+        genres[2].classList.remove("active-btn");
+        genres[3].classList.remove("active-btn");
+        genres[4].classList.remove("active-btn");
+        genres[5].classList.remove("active-btn");
+      } else if (genre === genres[2]) {
+        genre.classList.add("active-btn");
+        genres[0].classList.remove("active-btn");
+        genres[1].classList.remove("active-btn");
+        genres[3].classList.remove("active-btn");
+        genres[4].classList.remove("active-btn");
+        genres[5].classList.remove("active-btn");
+      } else if (genre === genres[3]) {
+        genre.classList.add("active-btn");
+        genres[0].classList.remove("active-btn");
+        genres[1].classList.remove("active-btn");
+        genres[2].classList.remove("active-btn");
+        genres[4].classList.remove("active-btn");
+        genres[5].classList.remove("active-btn");
+      } else if (genre === genres[4]) {
+        genre.classList.add("active-btn");
+        genres[0].classList.remove("active-btn");
+        genres[1].classList.remove("active-btn");
+        genres[2].classList.remove("active-btn");
+        genres[3].classList.remove("active-btn");
+        genres[5].classList.remove("active-btn");
+      } else if (genre === genres[5]) {
+        genre.classList.add("active-btn");
+        genres[0].classList.remove("active-btn");
+        genres[1].classList.remove("active-btn");
+        genres[2].classList.remove("active-btn");
+        genres[3].classList.remove("active-btn");
+        genres[4].classList.remove("active-btn");
       }
+
+      //   Showing and hiding movies based on their genre selected:
+      movies.forEach((movie) => {
+        if (movie.classList.contains(genreType)) {
+          movie.animate(
+            [{ opacity: "0" }, { opacity: "0.5" }, { opacity: "1" }],
+            {
+              duration: 1000,
+              fill: "forwards",
+            }
+          );
+          movie.style.display = "block";
+        } else if (genreType === "all") {
+          movie.style.display = "block";
+          movie.animate(
+            [{ opacity: "0" }, { opacity: "0.5" }, { opacity: "1" }],
+            {
+              duration: 1000,
+              fill: "forwards",
+            }
+          );
+        } else {
+          movie.style.display = "none";
+        }
+      });
     });
   });
-});
+}
+
+filterMovies();
